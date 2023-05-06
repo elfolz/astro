a5b817c1f63ae74233fbc8fb314c35605da238e8
Luiz <lhvalentim@outlook.coom>
2023-05-06 16:29:45 -0300

    fix sw

--- service-worker.js
+++ service-worker.js
@@ -3,38 +3,21 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
-	event.respondWith(caches.open('astro').then(cache => {
-		return cache.match(event.request)
-		.then(cachedResponse => {
-			let cachedFile = cachedResponse?.clone().blob()
-			let fetchedResponse
-			if (cachedFile) {
-				cachedFile.then(response => {
-					if (response?.size >= 50000) return cachedResponse
-					fetchedResponse = fetchNewData(event, cache, response)
+	event.respondWith(
+		caches.open('astro')
+		.then(cache => {
+			return cache.match(event.request)
+			.then(cachedResponse => {
+				let fetchedResponse = fetch(event.request)
+				.then(networkResponse => {
+					if (networkResponse.status == 200) cache.put(event.request, networkResponse.clone())
+					return networkResponse
 				})
-			} else {
-				fetchedResponse = fetchNewData(event, cache)
-			}
-			return cachedResponse || fetchedResponse
-		})
-	}))
-})
-
-function fetchNewData(event, cache, cachedFile) {
-	return fetch(event.request)
-	.then(networkResponse => {
-		if (networkResponse.status == 200) cache.put(event.request, networkResponse.clone())
-		if (cachedFile && event.clientId) {
-			networkResponse.clone().blob()
-			.then(response => {
-				if (response.size == cachedFile.size) return
-				self.clients.get(event.clientId)
-				.then(client => {
-					client?.postMessage('update')
-				})
+				return cachedResponse || fetchedResponse
 			})
-		}
-		return networkResponse
-	})
-}
+		})
+		.catch(() => {
+			return fetch(event.request)
+		})
+	)
+})
